4aeaf60dcc6024239af910ff8d3336687ce081bf
rob051815 <rob05182015@gmail.com>
2022-06-04 06:36:33 -0300

    Adds cors; adds tests with jest

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 
+import cors from 'cors';
 import express from 'express'
 import { routes } from './routes';
 
@@ -10,6 +11,7 @@ const app = express();
 // Patch = Update a single entity data
 // DELETE = Delete data...
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
