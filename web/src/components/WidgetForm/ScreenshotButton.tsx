a6fdadc9f6797ec52dcc04ce64cbb0b2ff525bdf
rob051815 <rob05182015@gmail.com>
2022-06-03 21:57:47 -0300

    mend

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -44,6 +44,7 @@ export function ScreenshotButton({
   return (
     <button
       type="button"
+      onClick={handleTakeScreenshot}
       className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
       {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100" />}
